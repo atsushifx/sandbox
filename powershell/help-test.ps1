8605aa9acc3040fedef84604873491da5233c277
Atsushi Furukawa <atsushifx@aglabo.com>
2021-11-26 02:14:13 +0900

    .gitignoreの改行コードを変更 パラメータ、コメントチェック用のpowershell scriptを追加

---
+++ powershell/help-test.ps1
@@ -0,0 +1,24 @@
+###
+#  display help only script
+#
+# @author Atsushi Furukawa <atsushifx@aglabo.com>
+# @date 2021/11/26
+# @version 1.0.0
+#
+
+<#
+  .SYNOPSYS
+  test powershell script with parameter. only help option defined
+
+  .DESCRIPTION
+
+  .EXAMPLE
+  help-test
+  do nothing
+
+  .EXAMPLE
+  help-test [-help|-?|-h]
+  display this help.
+
+#>
+
