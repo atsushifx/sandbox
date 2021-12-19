ecf7098f71fac751f203702506ab6aa9987a21a4
Atsushi Furukawa <atsushifx@aglabo.com>
2021-12-20 01:11:21 +0900

    .gitignore   Visual Studio Code用の設定を追加
    libs/commonUtils.ps1
      新しく、環境変数設定関数を作成開始
    
    Visual Studio Codeでpowershellスクリプトを編集するために各種設定ファイルを追加

--- powershell/libs/commonUtils.ps1
+++ powershell/libs/commonUtils.ps1
@@ -7,52 +7,15 @@ class aglabo
 }
 
 <#
- # static function class for check user role
- #
- # check current user is elevated as 'Administrator'
- #
- #
+  .SYNOPSIS
+    環境変数を設定する。(セッションおよびglobal)
  #>
-class userPrincipal : aglabo
-{
-	hidden static [Security.Principal.WindowsPrincipal] $principal = $null;
-
-	hidden static userPrincipal()
-	{
-		if ([userPrincipal]::principal -eq $null) {
-			[userPrincipal]::principal = getPrincipal
-		}
-	}
-
-
-	# get current windows principal
-	hidden static [Security.Principal.WindowsPrincipal] getPrincipal()
-	{
-		$id = [Security.Principal.WindowsIdentity]::GetCurrent()
-		return ([Security.Principal.WindowsPrincipal] $id)
-	}
-
-	<#
-      .SYNOPSIS
-        check current user has role parameter
-
-      .PARAMETER
-        $role
-        user role (Administrator, User, ...)
-	#>
-	static [bool] hasRole([Security.Principal.WindowsBuiltInRole] $role)
-	{
-		return [userPrincipal]::principal.IsInRole($role)
-	}
-
-	<#
-	  .SYNOPSIS
-        check user as 'Administrator'\
-    #>
-	static [bool]  isAdmin()
-	{
-		return [userPrincipal]::hasRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-	}
-}
-
-
+ function global:setGlobalEnv()
+ {
+   param (
+      [Parameter(Position=0, Mandatory)]
+      [ValidateSet('User', 'Machine')]
+      [string]
+        $envType = 'User'
+   )
+ }
